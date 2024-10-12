065340375ea2762fc6e825366d796e0a1615b2a2
Lewis Davies <lewisastondavies1@gmail.com>
2024-10-12 23:36:58 +0100

    V0.0.2

---
+++ pyLoginSystem/README.md
@@ -0,0 +1,45 @@
+
+# Python Package Documentation
+
+This Python package contains several scripts that handle different utility tasks such as version checking, password encryption, and database preparation. Below is an overview of each module and its usage.
+
+## Modules
+
+### 1. `check_ver.py`
+This script checks the current version of the software or package being used. It ensures that the user is running the correct version as specified.
+
+**Usage:**
+```bash
+python check_ver.py
+```
+
+### 2. `password_encrypter.py`
+This script provides functionality for encrypting passwords securely. It uses appropriate encryption algorithms to ensure password security for your application.
+
+**Usage:**
+```bash
+python password_encrypter.py
+```
+
+### 3. `prepare_database.py`
+This script is used to set up and prepare a database for use. It might include creating necessary tables, populating initial data, or setting up database configurations.
+
+**Usage:**
+```bash
+python prepare_database.py
+```
+
+## Installation
+
+To use these scripts, you can clone the repository and ensure that you have Python installed on your system.
+
+```bash
+git clone <repository-url>
+cd <repository-directory>
+```
+
+## License
+This project is licensed under the MIT License. See the `LICENSE` file for more information.
+
+## Contributing
+Feel free to open issues or pull requests for improvements and bug fixes.
--- pyLoginSystem/pyLoginSystem/prepare_database.py
+++ pyLoginSystem/pyLoginSystem/prepare_database.py
@@ -1,10 +1,43 @@
 import sqlalchemy as sql
-from password_encrypter import encrypt_password as encrypt
 
 # WARNING: Database will be reset (even if this file is only imported). Check that RESET is set 
 # to False before running this file.
 
 RESET = False
+
+def encrypt(password):
+    # For every character in the password
+    # Get the ASCII value of the character
+    # The first characters value goes up one
+    # The second goes down one
+    # And so on
+    
+    # Convert the ASCII value to a character
+    # Concatenate all the characters together
+    # Return the encrypted password
+    
+    # DISCLAIMER: Don't put any real passwords into this program (as it is open source,
+    #             and anyone can decrypt the passwords). I AM NOT RESPONSIBLE FOR ANY
+    #             DATA LEAKS OR DAMAGE CAUSED BY THIS PROGRAM.
+    
+    # README:     Change the encryption method BEFORE publishing the program for production.
+    #             This file should ALWAYS be kept top secret. If you leak this file, users
+    #             can see exactly how their passwords are encrypted, and can decrypt them.
+    #             (This will be a huge security risk).
+    
+    
+    encrypted_password = ""
+    
+    for letter in password:
+        ascii_value = ord(letter)
+        if ascii_value % 2 == 0:
+            encrypted_password += chr(ascii_value + 8)
+        else:
+            encrypted_password += chr(ascii_value - 5)
+    
+    del password
+    return encrypted_password
+
 
 def create_database():
     db = sql.create_engine('sqlite:///data.db')
